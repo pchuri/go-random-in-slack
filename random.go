6c24e7e7422d969e8a74e2ff5476168eb94232b9
박희철 <hcpark@afewgoodsoft.com>
2015-04-21 15:29:49 +0900

    query fix

--- random.go
+++ random.go
@@ -7,8 +7,6 @@ import (
 	"net/http"
 	"os"
 	"time"
-
-	"github.com/lib/pq"
 )
 
 var cfg *Config
@@ -36,11 +34,6 @@ func randomHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Connect Database Failed : %v", err)
 	}
 
-	url := os.Getenv("DATABASE_URL")
-	connection, _ := pq.ParseURL(url)
-	connection += " sslmode=require"
-
-	fmt.Fprintln(w, connection)
 	fmt.Fprintf(w, "INSERT INTO random_logs(username, random) VALUES (%v, %v)\n", username, randValue)
 
 	err = InsertRandomToDB(db, username, randValue)
